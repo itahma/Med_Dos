39313ac3706a32ffc581a6e329ae172750c428ae
itahmad <iteahmad2001@gmail.com>
2024-04-30 12:58:52 -0500

    V5_

--- lib/features/auth/presentation/secreen/Onboarding1_splash.dart
+++ lib/features/auth/presentation/secreen/Onboarding1_splash.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:med_dos/core/utils/app_assets.dart';
+import 'package:med_dos/core/utils/widget/customimage.dart';
 
 class OnBoarding1Screen extends StatelessWidget {
   const OnBoarding1Screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return    SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 140),
+              child: CustomImage(
+                  imagePath: AppAssets.Onboarding1,
+                w: 340,
+                h: 302,
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            const Padding(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                maxLines: 2,
+                "معنا ستكون على إطلاع دائم عن كل ما هو جديد في عالم الطب"
+              ,style:TextStyle(
+                fontSize: 20,
+                color: Colors.blue,
+                fontWeight:FontWeight.bold,
+              ),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            ElevatedButton(onPressed: (){},
+                child: Text("التالي")),
+          ],
+        ),
 
+      ),
     );
   }
 }
